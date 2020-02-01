ab10d1a30f76d35350efec8c51f855fbe719804b
novice5991 <novice5991@gmail.com>
2020-02-02 04:45:02 +0530

    4 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppMaterialModule } from './app-material.module';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule, AppMaterialModule ],
+  declarations: [ AppComponent, TopHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
